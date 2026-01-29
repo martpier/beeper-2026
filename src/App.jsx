50be1ec0cca8a59700ab00c64a32f8613b28d4fb
martpier <martin.pierret@datadoghq.com>
2026-01-29 08:58:46 +0100

    --wip-- [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router";
 import UserPage from './pages/UserPage';
 import { useAuth0 } from '@auth0/auth0-react';
+import HomePage from './pages/HomePage';
 
 function App() {
   const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
@@ -18,7 +19,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<UserPage />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
         <Route path="/user" element={<UserPage />}></Route>
       </Routes>
     </BrowserRouter>
--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,12 @@ createRoot(document.getElementById('root')).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        scope: 'openid profile email offline_access',
       }}
-      cacheLocation="localstorage"
       useRefreshTokens={true}
+      cacheLocation="localstorage"
     >
       <App />
     </Auth0Provider>
